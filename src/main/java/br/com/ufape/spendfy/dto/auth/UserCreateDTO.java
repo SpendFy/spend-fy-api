0c1b933b90dee01f8846fa0027665c0be54d98bb
Lucas210301 <lucastchaikovksy@hotmail.com>
2026-03-17 00:46:47 -0300

    feat: add User DTOs with validation

---
+++ src/main/java/br/com/ufape/spendfy/dto/auth/UserCreateDTO.java
@@ -0,0 +1,26 @@
+package br.com.ufape.spendfy.dto.auth;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserCreateDTO {
+    
+    @NotBlank(message = "Name is required")
+    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    private String name;
+    
+    @NotBlank(message = "Email is required")
+    @Email(message = "Email must be valid")
+    private String email;
+    
+    @NotBlank(message = "Password is required")
+    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
+    private String password;
+}
---
+++ src/main/java/br/com/ufape/spendfy/dto/auth/UserResponseDTO.java
@@ -0,0 +1,17 @@
+package br.com.ufape.spendfy.dto.auth;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import java.time.LocalDateTime;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class UserResponseDTO {
+    private String id;
+    private String name;
+    private String email;
+    private Boolean active;
+    private LocalDateTime createdAt;
+}
