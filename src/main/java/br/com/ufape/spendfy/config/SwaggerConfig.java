a2e925219f3642f4b97525f7bbe988493384b827
Lucas210301 <lucastchaikovksy@hotmail.com>
2026-03-17 01:02:58 -0300

    feat: add Swagger/OpenAPI configuration

--- src/main/java/br/com/ufape/spendfy/config/SwaggerConfig.java
+++ src/main/java/br/com/ufape/spendfy/config/SwaggerConfig.java
@@ -1,28 +1,28 @@
 package br.com.ufape.spendfy.config;
 
-import io.swagger.v3.oas.models.Components;
 import io.swagger.v3.oas.models.OpenAPI;
 import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
+import io.swagger.v3.oas.models.info.Contact;
+import io.swagger.v3.oas.models.info.License;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class SwaggerConfig {
-
+    
     @Bean
     public OpenAPI customOpenAPI() {
         return new OpenAPI()
-                .info(new Info()
-                        .title("SpendFy API")
-                        .version("1.0.0")
-                        .description("API REST para gestão financeira simples e inteligente"))
-                .components(new Components()
-                        .addSecuritySchemes("bearerAuth",
-                                new SecurityScheme()
-                                        .type(SecurityScheme.Type.HTTP)
-                                        .scheme("bearer")
-                                        .bearerFormat("JWT")
-                                        .description("Token JWT para autenticação. Use o endpoint /api/auth/login para obter o token.")));
+            .info(new Info()
+                .title("SpendFy API")
+                .version("1.0.0")
+                .description("API para gestão financeira pessoal - Personal Finance Management API")
+                .contact(new Contact()
+                    .name("SpendFy Team")
+                    .email("support@spendfy.com")
+                    .url("https://github.com/SpendFy"))
+                .license(new License()
+                    .name("MIT")
+                    .url("https://opensource.org/licenses/MIT")));
     }
 }
