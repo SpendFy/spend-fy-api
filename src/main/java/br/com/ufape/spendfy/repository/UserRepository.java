2f39359d3085c88d3a11a657a2c22ee4bc61d4fa
Lucas Tchaikovsky dos Santos Rodrigues <lucastchaikovsky@hotmail.com>
2026-03-17 21:47:13 -0300

    Change UserRepository ID type from String to Long
    mudanças e implementações feitas para compor a nota da 3ª V.A

--- src/main/java/br/com/ufape/spendfy/repository/UserRepository.java
+++ src/main/java/br/com/ufape/spendfy/repository/UserRepository.java
@@ -1,12 +1,13 @@
 package br.com.ufape.spendfy.repository;
 
-import br.com.ufape.spendfy.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import br.com.ufape.spendfy.entity.User;
+
 import java.util.Optional;
 
 @Repository
-public interface UserRepository extends JpaRepository<User, String> {
+public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByEmail(String email);
     boolean existsByEmail(String email);
 }
