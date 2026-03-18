43469b5c29b746e5b46cfb01565dc445ce483af6
Lucas Tchaikovsky dos Santos Rodrigues <lucastchaikovsky@hotmail.com>
2026-03-17 21:57:08 -0300

    Add constructor with message and cause to exception
    mudanças e implementações feitas para compor a nota da 3ª V.A

--- src/main/java/br/com/ufape/spendfy/exception/ResourceNotFoundException.java
+++ src/main/java/br/com/ufape/spendfy/exception/ResourceNotFoundException.java
@@ -1,11 +1,12 @@
 package br.com.ufape.spendfy.exception;
 
 public class ResourceNotFoundException extends RuntimeException {
+
     public ResourceNotFoundException(String message) {
         super(message);
     }
-    
-    public ResourceNotFoundException(String resourceName, String fieldName, Object fieldValue) {
-        super(String.format("%s not found with %s: %s", resourceName, fieldName, fieldValue));
+
+    public ResourceNotFoundException(String message, Throwable cause) {
+        super(message, cause);
     }
 }
