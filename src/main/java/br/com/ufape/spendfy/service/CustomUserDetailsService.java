ff1b8ac86cc59793e497a950d11993524f14302d
Lucas Tchaikovsky dos Santos Rodrigues <lucastchaikovsky@hotmail.com>
2026-03-17 21:42:40 -0300

    Refactor CustomUserDetailsService to use UserRepository
    mudanças e implementações feitas para compor a nota da 3ª V.A

--- src/main/java/br/com/ufape/spendfy/service/CustomUserDetailsService.java
+++ src/main/java/br/com/ufape/spendfy/service/CustomUserDetailsService.java
@@ -1,21 +1,41 @@
 package br.com.ufape.spendfy.service;
 
-import br.com.ufape.spendfy.repository.UsuarioRepository;
-import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
+import br.com.ufape.spendfy.entity.User as UserEntity;
+import br.com.ufape.spendfy.repository.UserRepository;
+
+import java.util.Collection;
+import java.util.Collections;
 
 @Service
-@RequiredArgsConstructor
 public class CustomUserDetailsService implements UserDetailsService {
 
-    private final UsuarioRepository usuarioRepository;
+    @Autowired
+    private UserRepository userRepository;
 
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-        return usuarioRepository.findByEmail(username)
-                .orElseThrow(() -> new UsernameNotFoundException("Usuário não encontrado: " + username));
+        UserEntity user = userRepository.findByEmail(username)
+            .orElseThrow(() -> new UsernameNotFoundException("User not found with email: " + username));
+
+        Collection<GrantedAuthority> authorities = Collections
+            .singletonList(new SimpleGrantedAuthority("ROLE_USER"));
+
+        return User.builder()
+            .username(user.getEmail())
+            .password(user.getPassword())
+            .authorities(authorities)
+            .accountExpired(false)
+            .accountLocked(false)
+            .credentialsExpired(false)
+            .disabled(false)
+            .build();
     }
 }
