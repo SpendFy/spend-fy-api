7048813abb8fd2d0cb33187ca631f5ab359c7c1d
Lucas Tchaikovsky dos Santos Rodrigues <lucastchaikovsky@hotmail.com>
2026-03-17 21:48:40 -0300

    Rename CategoriaRepository to CategoryRepository
    mudanças e implementações feitas para compor a nota da 3ª V.A

--- src/main/java/br/com/ufape/spendfy/repository/CategoriaRepository.java
+++ src/main/java/br/com/ufape/spendfy/repository/CategoriaRepository.java
@@ -1,16 +1,15 @@
 package br.com.ufape.spendfy.repository;
 
-import br.com.ufape.spendfy.entity.Categoria;
-import br.com.ufape.spendfy.entity.CategoriaType;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import br.com.ufape.spendfy.entity.Category;
+import br.com.ufape.spendfy.entity.User;
+
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface CategoriaRepository extends JpaRepository<Categoria, String> {
-    List<Categoria> findByUserId(String userId);
-    List<Categoria> findByUserIdAndType(String userId, CategoriaType type);
-    Optional<Categoria> findByIdAndUserId(String id, String userId);
-    boolean existsByUserIdAndName(String userId, String name);
+public interface CategoryRepository extends JpaRepository<Category, Long> {
+    List<Category> findByUser(User user);
+    List<Category> findByUserAndType(User user, Category.CategoryType type);
+    List<Category> findByUserIdOrderByCreatedAtDesc(Long userId);
 }
