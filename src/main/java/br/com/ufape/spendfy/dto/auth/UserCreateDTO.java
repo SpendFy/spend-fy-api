42497acd77a4b3b787c3192d9e7370b7616bdfe0
Lucas Tchaikovsky dos Santos Rodrigues <lucastchaikovsky@hotmail.com>
2026-03-17 21:51:26 -0300

    Refactor UserCreateDTO to UserDTO with new fields
    mudanças e implementações feitas para compor a nota da 3ª V.A

--- src/main/java/br/com/ufape/spendfy/dto/auth/UserCreateDTO.java
+++ src/main/java/br/com/ufape/spendfy/dto/auth/UserCreateDTO.java
@@ -1,26 +1,32 @@
-package br.com.ufape.spendfy.dto.auth;
+package br.com.ufape.spendfy.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
-import lombok.AllArgsConstructor;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
-public class UserCreateDTO {
-    
+@Builder
+public class UserDTO {
+
+    private Long id;
+
     @NotBlank(message = "Name is required")
-    @Size(min = 3, max = 100, message = "Name must be between 3 and 100 characters")
+    @Size(min = 2, max = 100, message = "Name must be between 2 and 100 characters")
     private String name;
-    
+
     @NotBlank(message = "Email is required")
     @Email(message = "Email must be valid")
     private String email;
-    
-    @NotBlank(message = "Password is required")
-    @Size(min = 8, max = 50, message = "Password must be between 8 and 50 characters")
-    private String password;
+
+    private LocalDateTime createdAt;
+
+    private LocalDateTime updatedAt;
 }
