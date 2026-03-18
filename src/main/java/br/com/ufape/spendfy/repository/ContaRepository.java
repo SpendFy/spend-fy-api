26c2e574346c8653748ec1f7ca51ef9879304f7b
Lucas Tchaikovsky dos Santos Rodrigues <lucastchaikovsky@hotmail.com>
2026-03-17 21:47:52 -0300

    Rename ContaRepository to AccountRepository and update methods
    mudanças e implementações feitas para compor a nota da 3ª V.A

--- src/main/java/br/com/ufape/spendfy/repository/ContaRepository.java
+++ src/main/java/br/com/ufape/spendfy/repository/ContaRepository.java
@@ -1,15 +1,14 @@
 package br.com.ufape.spendfy.repository;
 
-import br.com.ufape.spendfy.entity.Conta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import br.com.ufape.spendfy.entity.Account;
+import br.com.ufape.spendfy.entity.User;
+
 import java.util.List;
-import java.util.Optional;
 
 @Repository
-public interface ContaRepository extends JpaRepository<Conta, String> {
-    List<Conta> findByUserId(String userId);
-    List<Conta> findByUserIdAndActiveTrue(String userId);
-    Optional<Conta> findByIdAndUserId(String id, String userId);
-    boolean existsByUserIdAndName(String userId, String name);
+public interface AccountRepository extends JpaRepository<Account, Long> {
+    List<Account> findByUser(User user);
+    List<Account> findByUserIdOrderByCreatedAtDesc(Long userId);
 }
