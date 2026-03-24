c36da60f78e9fa697dc0b7a8a7f0dc176c65d732
Pedro Augusto Braga Medeiros <pedro.medeiros@demarco.com.br>
2026-03-24 13:50:56 -0300

    test: adiciona @ActiveProfiles(test) ao SpendfyApplicationTests para evitar bloqueio do H2

--- src/test/java/br/com/ufape/spendfy/SpendfyApplicationTests.java
+++ src/test/java/br/com/ufape/spendfy/SpendfyApplicationTests.java
@@ -2,8 +2,10 @@ package br.com.ufape.spendfy;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class SpendfyApplicationTests {
 
 	@Test
